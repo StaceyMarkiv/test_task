5019da12caffac4275699dd9f04c34c53bce6be9
Stacey Markiv <stasy-9876@mail.ru>
2022-11-11 01:19:55 +0300

    Delete test_results.py

--- test_results.py
+++
@@ -1,9 +0,0 @@
-test_samples = {
-    "v4bc3d5e": "vvvvbcccddddde",
-    "abcd": "abcd",
-    "45": "",   # error
-    "": "",
-    r"qwe\4\5": "qwe45",
-    r"qwe\45": "qwe44444",
-    r"qwe\\5 ": r"qwe\\\\\ "
-}
